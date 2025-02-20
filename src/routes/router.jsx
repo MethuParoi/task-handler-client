e6c1fe04196f5ecdf49ca16751fe4085606e3a26
MethuParoi <paroimethu@gmail.com>
2025-02-21 00:54:35 +0530

    initialize project with React, Vite, Tailwind CSS, and DaisyUI; add basic structure and components

---
+++ src/routes/router.jsx
@@ -0,0 +1,36 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import NotFound from "../pages/NotFound";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      
+    ],
+  },
+ 
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Register />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
+
+export default router;
